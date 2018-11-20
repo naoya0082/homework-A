a06a825e5ff087513edc8b68ef318b805eefef59
naoya0082 <6ds3f7@gmail.com>
2018-11-20 09:53:27 +0900

    A-5 完了 もっとスマートな出力方法がありそう

--- work.py
+++ work.py
@@ -12,3 +12,8 @@ kazuma_info = ["Kazuma", "Takahashi", 35]
 members = ["Kazuma", "Makoto", "Ohira"]
 print(members[0])
 print(members[1])
+
+# A-5 要素へのアクセスとフォーマット
+# "Name: Takahashi Kazuma, Age: 35"と出力
+kazuma_info = ["Kazuma", "Takahashi", 35]
+print(f"Name: {kazuma_info[1]} {kazuma_info[0]}, Age: {kazuma_info[2]}")
