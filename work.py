bd498dde6203670b23e3f7a50e54d089ea349b0b
naoya0082 <6ds3f7@gmail.com>
2018-11-20 09:52:24 +0900

    A-4 完了 1回のprintで2つの要素を出力したい

---
+++ work.py
@@ -0,0 +1,14 @@
+# A-1 文字列のリスト
+users = ["Bob", "Tom", "Ken"]
+
+# A-2 整数のリスト
+int_numbers = [1, 2, 3, 4, 5]
+
+# A-3 要素のデータが異なるリスト
+kazuma_info = ["Kazuma", "Takahashi", 35]
+
+# A-4 要素へのアクセス
+# Kazuma Makoto を出力
+members = ["Kazuma", "Makoto", "Ohira"]
+print(members[0])
+print(members[1])
