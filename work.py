a92ad87381b92b08a18d4ca95a5213a492ab3bee
naoya0082 <6ds3f7@gmail.com>
2018-11-20 10:26:05 +0900

    A-8 完了 リストの中の更に細かい出力は、list[][] で実行できる
    list = ["naoya", "Takahashi"]
    print(list[0][0]) -> "n"
    
    リストの中にリストがあっても同じ
    list = [["Naoya"],["Takahashi"]]
    print(list[0][0]) -> "Naoya"

--- work.py
+++ work.py
@@ -7,23 +7,45 @@ int_numbers = [1, 2, 3, 4, 5]
 # A-3 要素のデータが異なるリスト
 kazuma_info = ["Kazuma", "Takahashi", 35]
 
+print("--------------A-4--------------")
+
 # A-4 要素へのアクセス
 # Kazuma Makoto を出力
 members = ["Kazuma", "Makoto", "Ohira"]
 print(members[0])
 print(members[1])
 
+print("--------------A-5--------------")
+
 # A-5 要素へのアクセスとフォーマット
 # "Name: Takahashi Kazuma, Age: 35"と出力
 kazuma_info = ["Kazuma", "Takahashi", 35]
 print(f"Name: {kazuma_info[1]} {kazuma_info[0]}, Age: {kazuma_info[2]}")
+
+print("--------------A-6--------------")
 
 # A-6 forによるループその1
 odd_numbers = [1, 3, 5, 7, 9]
 for loop in odd_numbers:
     print(loop)
 
+print("--------------A-7--------------")
+
 # A-7 forによるループその2
 even_numbers = [2, 4, 6, 8]
 for loop2 in even_numbers:
     print(loop2 * 2)
+
+print("--------------A-8--------------")
+
+# A-8 リストを要素に持つリスト
+# 以下を出力
+# "Name: Kazuma, Age: 35"
+# "Name: Tom, Age: 57"
+# "Name: Bob, Age: 77"
+users_info = [["Kazuma", 35],
+              ["Tom", 57],
+              ["Bob", 77]]
+print(f"Name: {users_info[0][0]}, Age: {users_info[0][1]}")
+print(f"Name: {users_info[1][0]}, Age: {users_info[1][1]}")
+print(f"Name: {users_info[2][0]}, Age: {users_info[2][1]}")
